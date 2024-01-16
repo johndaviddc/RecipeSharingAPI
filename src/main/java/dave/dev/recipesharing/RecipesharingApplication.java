6330ee8ee047712a236bbd13efec28191735b199
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-16 10:24:02 +0800

    recipe shring api

---
+++ src/main/java/dave/dev/recipesharing/RecipesharingApplication.java
@@ -0,0 +1,13 @@
+package dave.dev.recipesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dave/dev/recipesharing/RecipesharingApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.recipesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
