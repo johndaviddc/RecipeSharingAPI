799be6aedbdd9fde20a46f7f5667aecd7b84b61f
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-25 08:51:14 +0800

    added recipe sharing exception

---
+++ src/main/java/dave/dev/recipesharing/exception/RecipeServiceException.java
@@ -0,0 +1,7 @@
+package dave.dev.recipesharing.exception;
+
+public class RecipeServiceException extends RuntimeException {
+    public RecipeServiceException(String message) {
+        super(message);
+    }
+}
